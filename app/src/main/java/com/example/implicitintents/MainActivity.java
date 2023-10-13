b2893d0a5587ab1a804e7d6ee16c53dce33615cb
Víctor Mato Ramos <victormator5@gmail.com>
2023-10-13 16:31:49 +0200

    Añadido el Intent para lanzar la página web

--- app/src/main/java/com/example/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -2,18 +2,43 @@ package com.example.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Asignar a la variable el valor del EditText
+        mWebsiteEditText = findViewById(R.id.website_edittext);
     }
 
+    @SuppressLint("QueryPermissionsNeeded")
     public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+
+        // Codifica y analiza el String en un objeto Uri
+        Uri webpage = Uri.parse(url);
+
+        // Aquí se especifica una accion y los datos para esa accion
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Busca una actividad para entregar la intención y comenzar la actividad
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "¡No puedo con esto!");
+        }
     }
 
     public void openLocation(View view) {
